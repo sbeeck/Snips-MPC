e865011e5dfe88c4731bef366128e14f9d3b6147
sbeeck <60064488+sbeeck@users.noreply.github.com>
2020-01-29 07:37:56 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+PYTHON=$(command -v python3)
+VENV=venv
+
+if [ -f "$PYTHON" ]
+then
+
+    if [ ! -d $VENV ]
+    then
+        # Create a virtual environment if it doesn't exist.
+        $PYTHON -m venv $VENV
+    else
+        if [ -e $VENV/bin/python2 ]
+        then
+            # If a Python2 environment exists, delete it first
+            # before creating a new Python 3 virtual environment.
+            rm -r $VENV
+            $PYTHON -m venv $VENV
+        fi
+    fi
+
+    # Activate the virtual environment and install requirements.
+    # shellcheck source=/dev/null
+    . $VENV/bin/activate
+    pip3 install -r requirements.txt
+
+else
+    >&2 echo "Cannot find Python 3. Please install it."
+fi
